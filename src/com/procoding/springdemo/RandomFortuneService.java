33756c3b8b10df81c0fab0ab2cc059dbc97c7ded
subhram biswas <subhrambiswas2014@gmail.com>
2022-04-06 18:31:07 +0530

    Testing rebase amd merge 5

--- src/com/procoding/springdemo/RandomFortuneService.java
+++ src/com/procoding/springdemo/RandomFortuneService.java
@@ -21,7 +21,7 @@ public class RandomFortuneService implements FortuneService {
 			"Learn full stack Technology",
 			"Be your own Boss!!",
 			"Life is hell",
-			"Testing rebase and merge and learn and grow"
+			"Testing rebase and merge and learn and grow and earn"
 	};
 	
 	// create a random number generator
